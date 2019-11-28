c8056cfbd577e5f089155b6459557d25f9fec68b
mesneym <mesney.mesney@gmail.com>
2019-11-28 17:20:32 -0500

    [MARKERS]Setting up main.cpp file

---
+++ src/main.cpp
@@ -0,0 +1,9 @@
+#include <ros/ros.h>
+#include <visualization_msgs/Marker.h>
+#include "nav_msgs/Odometry.h"
+
+
+int main(int argc, char *argv[]) {
+  ros::init(argc,argv,"vmarkers");
+  return 0;
+}
