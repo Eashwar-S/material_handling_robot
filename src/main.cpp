70baca042c8a2f345f818ea64f87481e1e6f791e
mesneym <mesney.mesney@gmail.com>
2019-11-28 21:05:02 -0500

    [MARKER][main.cpp]Updated main.cpp to call marker class

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,20 @@
 #include <ros/ros.h>
 #include <visualization_msgs/Marker.h>
 #include "nav_msgs/Odometry.h"
-
+#include "markers.h"
 
 int main(int argc, char *argv[]) {
   ros::init(argc,argv,"vmarkers");
+  ros::NodeHandle nh;
+   
+  Vmarkers vmarker;
+  vmarker.visualizeMarker(argc, argv);
+
   return 0;
 }
+
+
+
+
+ 
+
