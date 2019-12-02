c9691e693b3355ad22d124b85bef297b8576c196
mesneym <mesney.mesney@gmail.com>
2019-12-01 23:47:17 -0500

    [INT]Segregated classes to run as different nodes

---
+++ src/main2.cpp
@@ -0,0 +1,15 @@
+
+#include <visualization_msgs/Marker.h>
+#include "nav_msgs/Odometry.h"
+#include "location.hpp"
+
+int main(int argc, char *argv[]) {
+  ros::init(argc,argv,"vmarkers");
+  ros::NodeHandle nh;
+   
+  Location vmarker;
+  vmarker.visualizeLocation(argc, argv);
+
+  return 0;
+}
+
