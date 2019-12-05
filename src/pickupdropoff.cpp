57d5402c1563f6cffe3854cd1cddd6664c13e80c
mesneym <mesney.mesney@gmail.com>
2019-12-04 19:03:09 -0500

    [INT1][pickupdropoff.cpp]Modified file for integration station class

--- src/pickupdropoff.cpp
+++ src/pickupdropoff.cpp
@@ -4,21 +4,23 @@
 Pickupdropoff::~Pickupdropoff() {}
 Pickupdropoff::Pickupdropoff() {}
 
-bool Pickupdropoff::goToPickUpLocation(move_base_msgs::MoveBaseGoal &goal){
+bool Pickupdropoff::goToPickUpLocation(move_base_msgs::MoveBaseGoal &goal) {
   goal.target_pose.header.frame_id = "map";
   goal.target_pose.header.stamp = ros::Time::now();
+
    //define a position and orientation for the robot to reach
-  goal.target_pose.pose.position.x = -2;
-  goal.target_pose.pose.position.y = 1;
+    
+  goal.target_pose.pose.position.x = (s.getStations())[1].getX();
+  goal.target_pose.pose.position.y = (s.getStations())[1].getY();
   goal.target_pose.pose.orientation.w = 1.00;
   return true;
 }
 
-bool Pickupdropoff::goToDropOffLocation(move_base_msgs::MoveBaseGoal &goal){
+bool Pickupdropoff::goToDropOffLocation(move_base_msgs::MoveBaseGoal &goal) {
   goal.target_pose.header.frame_id = "map";
   goal.target_pose.header.stamp = ros::Time::now();
-  goal.target_pose.pose.position.x = 0;
-  goal.target_pose.pose.position.y = 0;
+  goal.target_pose.pose.position.x = (s.getStations())[2].getX();
+  goal.target_pose.pose.position.y = (s.getStations())[2].getY();
   goal.target_pose.pose.orientation.w = -1;
   return true;
 
