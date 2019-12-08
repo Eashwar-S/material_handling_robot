416066fa4600a8dbdcc473b5707602fa6efa27ef
mesneym <mesney.mesney@gmail.com>
2019-12-07 22:43:01 -0500

    [REFACTOR][Pickupdropoff.cpp]Adding and modifying relevant methods for cpp

--- src/pickupdropoff.cpp
+++ src/pickupdropoff.cpp
@@ -2,29 +2,54 @@
 
 
 Pickupdropoff::~Pickupdropoff() {}
-Pickupdropoff::Pickupdropoff(int startL,int endL) {
-  startLocation = startL;
-  endLocation = endL;
+
+Pickupdropoff::Pickupdropoff() {
 }
 
-bool Pickupdropoff::goToPickUpLocation(move_base_msgs::MoveBaseGoal &goal) {
-  // goal.target_pose.header.frame_id = "map";
-  // goal.target_pose.header.stamp = ros::Time::now();
 
-   //define a position and orientation for the robot to reach
-  goal.target_pose.pose.position.x = (s.getStations())[startLocation].getX();
-  goal.target_pose.pose.position.y = (s.getStations())[startLocation].getY();
-  goal.target_pose.pose.orientation.w = 1.00;
-  return true;
-}
+bool Pickupdropoff::updateLocation(move_base_msgs::MoveBaseGoal &goal,int target) {
 
-bool Pickupdropoff::goToDropOffLocation(move_base_msgs::MoveBaseGoal &goal) {
-  // goal.target_pose.header.frame_id = "map";
-  // goal.target_pose.header.stamp = ros::Time::now();
-  goal.target_pose.pose.position.x = (s.getStations())[endLocation].getX();
-  goal.target_pose.pose.position.y = (s.getStations())[endLocation].getY();
+
+   MoveBaseClient ac("move_base", true);
+   goal.target_pose.header.frame_id = "map";
+   goal.target_pose.header.stamp = ros::Time::now();
+  goal.target_pose.pose.position.x = (s.getStations())[target].getX();
+  goal.target_pose.pose.position.y = (s.getStations())[target].getY();
   goal.target_pose.pose.orientation.w = -1;
+
   return true;
 }
 
+
+bool Pickupdropoff::goToLocation(MoveBaseClient &ac,const move_base_msgs::MoveBaseGoal &goal) {
+  //Send the goal position and orientation for the robot to reach
+  ROS_INFO("Sending goal");
+  ac.sendGoal(goal);
+
+   //Wait an infinite time for the results
+  ac.waitForResult();
+  return ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED;
+}
+
+
+bool Pickupdropoff::goToPickAndDrop(move_base_msgs::MoveBaseGoal &goal,MoveBaseClient &ac, int startLocation, int endLocation) {
+  updateLocation(goal,startLocation);
+
+   //Check if the robot reached its 0st goal
+  if (goToLocation(ac,goal)) {
+    ROS_INFO("0st Goal is successfully reached");
+    ros::Duration(4.0).sleep(); // wait 5 sec to receive next goal
+    updateLocation(goal,endLocation);
+  
+    //Check if the robot reached its 1nd goal
+    if (goToLocation(ac,goal)) {
+       ROS_INFO("1nd Goal is successfully reached");
+       ros::Duration(4.0).sleep();
+    } 
+    else
+       ROS_INFO("The robot could not reach the second goal");
+  }
+  else
+    ROS_INFO("The robot could not reach the first goal");
+}
 
